dda0b80acfb6ee5d87673f0b8fad31a23b6dc741
wailovet <wailovet@163.com>
2016-01-11 16:47:24 +0800

    tests

--- tests/MediaTest.php
+++ tests/MediaTest.php
@@ -7,16 +7,17 @@
  * Time: 11:58
  */
 use Wailovet\wechat\Media;
+
 class MediaTest extends PHPUnit_Framework_TestCase
 {
     private $media;
 
     public function setUp()
     {
-        $corpid  = 'wxf6a65243b40d13ac';
+        $corpid = 'wxf6a65243b40d13ac';
         $corpsecret = 'G_4BogDOqC-nyEmAbpCf9mRx6ZC9O176gYUZGJdau_ZTkftQlTE4NW9zwayiAORJ';
 
-        $this->media = new Media($corpid,$corpsecret);
+        $this->media = new Media($corpid, $corpsecret);
     }
 
     public function tearDown()
@@ -29,9 +30,22 @@ class MediaTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(!empty($this->media));
     }
 
-    public function testCount(){
+    public function testCount()
+    {
         $data = $this->media->count(0);
         $this->assertTrue($data['errcode'] === 0);
-        $this->assertTrue(false);
+    }
+
+
+    public function testAll()
+    {
+        $media = $this->media;
+        file_put_contents(dirname(__FILE__) . "/test.txt", "test");
+        $data = $media->file(dirname(__FILE__) . "/test.txt")->type(Media::TYPE_FILE)->upload();
+        $this->assertTrue($data['type'] == "file" && !empty($data['media_id']));
+        $media_id = $data['media_id'];
+        $media->id($media_id)->download(dirname(__FILE__) . "/test2.txt");
+        $test2 = file_get_contents(dirname(__FILE__) . "/test2.txt");
+        $this->assertTrue($test2 == "test");
     }
 }
