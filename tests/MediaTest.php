5e712fa14ba9cf7bcee857fe2b8c4a5eebb19517
wailovet <wailovet@163.com>
2016-01-11 16:32:46 +0800

    tests

--- tests/MediaTest.php
+++ tests/MediaTest.php
@@ -9,6 +9,8 @@
 use Wailovet\wechat\Media;
 class MediaTest extends PHPUnit_Framework_TestCase
 {
+    private $media;
+
     public function setUp()
     {
     }
@@ -21,12 +23,16 @@ class MediaTest extends PHPUnit_Framework_TestCase
     {
         // test to ensure that the object from an fsockopen is valid
 
-        require_once(dirname(__FILE__)."/../autoload.php");
 
         $corpid  = 'wxf6a65243b40d13ac';
         $corpsecret = 'G_4BogDOqC-nyEmAbpCf9mRx6ZC9O176gYUZGJdau_ZTkftQlTE4NW9zwayiAORJ';
 
-        $media = new Media($corpid,$corpsecret);
-        $this->assertTrue(!empty($media));
+        $this->media = new Media($corpid,$corpsecret);
+        $this->assertTrue(!empty($this->media));
+    }
+
+    public function testCount(){
+
+        $this->media->count(0);
     }
 }
