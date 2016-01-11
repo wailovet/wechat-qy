c74c8a7c5a1ed7cea3b726c1f91ffdee386a4310
wailovet <wailovet@163.com>
2016-01-11 16:10:38 +0800

    tests

--- tests/MediaTest.php
+++ tests/MediaTest.php
@@ -16,10 +16,16 @@ class MediaTest extends PHPUnit_Framework_TestCase
     {
     }
 
-    public function testConnectionIsValid()
+    public function testIsInit()
     {
         // test to ensure that the object from an fsockopen is valid
 
         require_once(dirname(__FILE__)."/../autoload.php");
+
+        $corpid  = 'wxf6a65243b40d13ac';
+        $corpsecret = 'G_4BogDOqC-nyEmAbpCf9mRx6ZC9O176gYUZGJdau_ZTkftQlTE4NW9zwayiAORJ';
+
+        $media = new Media($corpid,$corpsecret);
+        $this->assertTrue($media);
     }
 }
